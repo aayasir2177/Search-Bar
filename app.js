06f3b5798482a6cbfd1399bbfd1912d6c6b2d642
Yasir <44633211+aayasir2177@users.noreply.github.com>
2022-11-22 17:54:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+(async () => {
+  // Fetching Data
+  const res = await fetch("https://dummyjson.com/users");
+  let data = await res.json();
+
+  // Getting Input
+  let searchBar = document.querySelector("[search-bar]");
+
+  // Getting Main Container
+  let dataContainer = document.querySelector("[data-container]");
+
+  // Getting Card Template
+  let cardTemplate = document.querySelector("template").content;
+
+  // Filtering Users
+  searchBar.addEventListener("input", filterUsers);
+
+  // Populating Users
+  searchBar.addEventListener("input", populateUsers);
+
+  // User Filter
+  function filterUsers() {
+    let searchTerm = searchBar.value.toLowerCase();
+
+    let filteredData = data.users.filter((user) =>
+      user.firstName.toLowerCase().includes(searchTerm)
+    );
+
+    console.log(searchTerm);
+
+    return filteredData;
+  }
+
+  // User Populator
+  function populateUsers() {
+    let filteredUsers = filterUsers();
+
+    dataContainer.textContent = "";
+
+    filteredUsers.forEach((user) => {
+      let card = cardTemplate.cloneNode(true),
+        cardName = card.querySelector("[card-name]"),
+        cardEmail = card.querySelector("[card-email]");
+      cardName = cardName.innerText = user.firstName;
+      cardEmail = cardEmail.innerText = user.email;
+
+      dataContainer.appendChild(card);
+
+      console.log(filteredUsers);
+    });
+  }
+})();
